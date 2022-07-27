fe7e5b8431185dbe444a32e556c0e7f98836e867
Magnus-Cosmos <39173355+Magnus-Cosmos@users.noreply.github.com>
2022-07-27 07:02:05 +0000

    Deploying to gh-pages from @ Magnus-Cosmos/IdleSpiral@719146f86099334203b124cf744939b30c8f625b 🚀

---
+++ _next/static/ddsMEKeTZDI_ZWCfSrmCb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb1608f2-c527f80217462a52.js","static/chunks/418-84688ac06cf42d4d.js","static/chunks/pages/index-a45f3718deb5e6be.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
